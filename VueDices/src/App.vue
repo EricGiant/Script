28104bb555071883ccf815eb291dc35705e57a11
EricGiant <ericspier@hotmail.com>
2023-06-13 16:07:45 +0200

    VUE3 dices pre code review
    this is VUE3 dices finished pre code review

---
+++ VueDices/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { reactive } from 'vue';
+
+const diceValues = reactive([]);
+const diceAmount = 8;
+
+function throwDice()
+{
+    diceValues.splice(0);
+    for(var i = 0; i < diceAmount; i++)
+    {
+        diceValues.push(Math.floor(Math.random() * 6) + 1);
+    }
+}
+
+</script>
+
+<template>
+    <button @click = "throwDice()">THROW</button>
+    <table v-if = "diceValues.length != 0">
+        <tr>
+            <th>Dice</th>
+            <th>Value</th>
+        </tr>
+        <div v-for = "(item, index) in diceValues">
+            <tr>
+                <th>{{index}}</th>
+                <th>{{item}}</th>
+            </tr>
+        </div>
+    </table>
+</template>
