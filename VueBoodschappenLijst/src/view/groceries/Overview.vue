f340e41458358d040373fb26211e6f7741184aba
EricGiant <ericspier@hotmail.com>
2023-06-21 14:05:28 +0200

    Added router to bootschappenlijst
    Added router from OP29 to bootschappenlijst

---
+++ VueBoodschappenLijst/src/view/groceries/Overview.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import GroceriesTable from "../../components/GroceriesTable.vue";
+
+const productList = [
+    {
+        name: "test",
+        price: 5,
+        amount: 5
+    },
+    {
+        name: "test2",
+        price: 4,
+        amount: 10
+    },
+    {
+        name: "test3",
+        price: 50,
+        amount: 1
+    }]
+
+</script>
+
+<template>
+    <GroceriesTable v-bind:productList = "productList"/>
+</template>
