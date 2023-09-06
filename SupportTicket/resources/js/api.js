5462a054dd2b3e56b8b7d0b8b6e5e86f12bf0432
Jeroen Postema <jeroen@script.nl>
2023-09-06 10:06:44 +0200

    add axios interceptor

---
+++ SupportTicket/resources/js/api.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const api = axios.create({
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+});
+
+// Add a request interceptor
+api.interceptors.request.use(
+    function (config) {
+        // Do something before request is sent
+        return config;
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor
+api.interceptors.response.use(
+    function (response) {
+        console.log("test intereptor");
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+    },
+    function (error) {
+        console.log("test intereptor error");
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+    }
+);
+
+export default api;
--- SupportTicket/resources/js/login/store/authentication.ts
+++ SupportTicket/resources/js/login/store/authentication.ts
@@ -1,11 +1,16 @@
-import axios from "axios";
+import axios from "../../api";
 import { login } from "../types/login";
 
 export const authenticateUser = async (formData: login) => {
-    const data = await axios.post("/api/authenticateUser", {email: formData.email, password: formData.password}).catch((error) => error.response.status);
+    const data = await axios
+        .post("/api/authenticateUser", {
+            email: formData.email,
+            password: formData.password,
+        })
+        .catch((error) => error.response.status);
     return data;
-}
+};
 
 export const logout = async () => {
     await axios.post("/api/logout");
-}
+};
